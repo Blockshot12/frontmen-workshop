6af9e438f6983462dc90a5e38466b196212e470f
Francois van Leersum <contact@blockshot.org>
2018-01-26 13:36:37 +0100

    Start solution

--- 15-EXERCISE.js
+++ 15-EXERCISE.js
@@ -5,3 +5,18 @@
  * and the first two Fibonacci numbers are 1 and 1.
  * E.g.: 1, 1, 2, 3, 5, 8, 13, ...
  */
+
+function fibonacci() {
+  // Inintialization
+  let prev = 1;
+  let prevprev = 1;
+  let i = 0;
+  return () => {
+    // move next
+    if (i++ < 2) {
+      return i;
+    } else {
+      
+    }
+  }
+}
